f5295ec47fef29187ccfccf1e13614fed7c3181e
viexher <124597095+viexher@users.noreply.github.com>
2023-05-17 15:31:06 +0100

    Add images, responsive menu for mobile devices and media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-document.getElementById('year').innerHTML = (new Date().getFullYear());
+document.getElementById("year").innerHTML = (new Date().getFullYear());
+
+
+/* Toggle menu on mobile */
+
+function menuToggle() {
+  const navMenu = document.querySelectorAll('.main-menu');
+
+  navMenu.forEach(navLinks => navLinks.classList.toggle('nav-toggle-show'));
+}
+
+document.querySelector('.toggle').addEventListener('click', menuToggle);
+
